90f7ff7977ab54317a560e422b4b3893ff0a1c0d
Daniela Leal <falfada@gmail.com>
2023-08-28 21:49:02 +1000

    Setting up slider and hero section

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,44 @@
-/**
+$(document).ready(function(){
+  /**
+ * Popup video.
+ */
+  $('.open-popup').on('click', function() {
+    $('.popup-overlay').fadeIn();
+  });
+
+
+  $('.close-popup').on('click', function() {
+    $('.popup-overlay').fadeOut();
+  });
+  /**
+ * Clients slider.
+ */
+  // $('.clients-slider').slick({
+  //   slidesToShow: 5,
+  //   slidesToScroll: 1,
+  //   infinite: true,
+  //   easing: 'linear',
+  // });
+  /**
+ * Our Work/Testimonials.
+ */
+  // $('.variable-width').slick({
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   infinite: true,
+  //   easing: 'linear',
+  // });
+  
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  /**
  * Header.
  */
 const header = document.querySelector(".header");
@@ -9,4 +49,21 @@ window.addEventListener("scroll", () => {
   } else {
     header.classList.remove("scrolled");
   }
+});
+/**
+ * Animations.
+ */
+const observer = new IntersectionObserver ((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    } else{
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
 });
