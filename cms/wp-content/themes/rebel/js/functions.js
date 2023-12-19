9858ab2d7ba028f106cc6d91f887e29a2e1e6497
Daniela Leal <falfada@gmail.com>
2023-12-19 20:59:32 +1100

    Form showing file inputs

--- cms/wp-content/themes/rebel/js/functions.js
+++ cms/wp-content/themes/rebel/js/functions.js
@@ -1,15 +1,26 @@
 $(document).ready(function () {
   /**
+   * Form Input.
+   */
+  $("#fileInput").change(function () {
+    let parentContainer = $(this).parent();
+    let labelForFile = parentContainer.siblings("label");
+    let textToDisplay = $("#fileInput")[0].files[0].name;
+    let newParagraph = `<p class="fileName">${textToDisplay}</p>`;
+    labelForFile.after(newParagraph);
+  });
+
+  /**
    * Lottie Animation.
    */
 
-  $(window).on('load', function () {
-    if ($('body.home').length) {
+  $(window).on("load", function () {
+    if ($("body.home").length) {
       let myVar = setTimeout(showPage, 3000);
 
       function showPage() {
-        document.getElementById('pageLoader').style.display = 'none';
-        document.getElementById('pageContent').style.display = 'block';
+        document.getElementById("pageLoader").style.display = "none";
+        document.getElementById("pageContent").style.display = "block";
       }
     }
   });
@@ -18,32 +29,36 @@ $(document).ready(function () {
    * Other Option Form.
    */
 
-  $('.wpcf7-form-control-wrap select').on('change', function () {
+  $(".wpcf7-form-control-wrap select").on("change", function () {
     const triggerValue = $(this).val();
     const grandparentContainer = $(this).closest("p");
-    if(triggerValue === 'Other'){
-      let otherOptionContainer = grandparentContainer.next().css({ display: "block" });
-    } else{
-      let otherOptionContainer = grandparentContainer.next().css({ display: "none" });
+    if (triggerValue === "Other") {
+      let otherOptionContainer = grandparentContainer
+        .next()
+        .css({ display: "block" });
+    } else {
+      let otherOptionContainer = grandparentContainer
+        .next()
+        .css({ display: "none" });
     }
   });
-  
+
   /**
    * Form Stepper.
    */
-  const steps = Array.from(document.querySelectorAll('.step'));
-  const stepNumbers = Array.from(document.querySelectorAll('.step-number'));
-  const nextButtons = Array.from(document.querySelectorAll('.next-button'));
-  const prevButtons = Array.from(document.querySelectorAll('.prev-button'));
+  const steps = Array.from(document.querySelectorAll(".step"));
+  const stepNumbers = Array.from(document.querySelectorAll(".step-number"));
+  const nextButtons = Array.from(document.querySelectorAll(".next-button"));
+  const prevButtons = Array.from(document.querySelectorAll(".prev-button"));
 
   let currentStep = 0;
 
   function updateProgress() {
     stepNumbers.forEach((number, index) => {
       if (index === currentStep) {
-        number.classList.add('active');
+        number.classList.add("active");
       } else {
-        number.classList.remove('active');
+        number.classList.remove("active");
       }
     });
   }
@@ -51,9 +66,9 @@ $(document).ready(function () {
   function showStep(step) {
     steps.forEach((s, index) => {
       if (index === step) {
-        s.style.display = 'block';
+        s.style.display = "block";
       } else {
-        s.style.display = 'none';
+        s.style.display = "none";
       }
     });
   }
@@ -63,7 +78,7 @@ $(document).ready(function () {
   updateProgress();
 
   nextButtons.forEach((button, index) => {
-    button.addEventListener('click', (e) => {
+    button.addEventListener("click", (e) => {
       e.preventDefault();
       currentStep++;
       if (currentStep >= steps.length) {
@@ -75,7 +90,7 @@ $(document).ready(function () {
   });
 
   prevButtons.forEach((button, index) => {
-    button.addEventListener('click', (e) => {
+    button.addEventListener("click", (e) => {
       e.preventDefault();
       currentStep--;
       if (currentStep < 0) {
@@ -89,7 +104,7 @@ $(document).ready(function () {
   /**
    * Table slider mobile.
    */
-  var tableMobile = new Swiper('.table.mobile.mySwiper', {});
+  var tableMobile = new Swiper(".table.mobile.mySwiper", {});
   /**
    * Highlight Animation.
    */
@@ -97,13 +112,13 @@ $(document).ready(function () {
   function startAnimation(entries, observer) {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add('animate');
+        entry.target.classList.add("animate");
         observer.unobserve(entry.target);
       }
     });
   }
 
-  const markElements = document.querySelectorAll('.mark');
+  const markElements = document.querySelectorAll(".mark");
 
   markElements.forEach((markElement) => {
     const observer = new IntersectionObserver(startAnimation, {
@@ -123,28 +138,28 @@ $(document).ready(function () {
       delay: 1,
       disableOnInteraction: false,
     },
-    slidesPerView: 'auto',
+    slidesPerView: "auto",
     speed: 6000,
     grabCursor: true,
     spaceBetween: 24,
     //mousewheelControl: true,
     //keyboardControl: true,
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
   };
 
   let swiperLogosSlider = new Swiper(
-    '.clients-logos.swiper-container',
+    ".clients-logos.swiper-container",
     swiperLogos
   );
   /**
    * Menu Burger.
    */
-  $('.burger').click(function () {
-    $(this).toggleClass('rotate');
-    $('.mobile-menu').slideToggle();
+  $(".burger").click(function () {
+    $(this).toggleClass("rotate");
+    $(".mobile-menu").slideToggle();
   });
   /**
    * Clients Slider 1.
@@ -155,20 +170,20 @@ $(document).ready(function () {
       delay: 1,
       disableOnInteraction: false,
     },
-    slidesPerView: 'auto',
+    slidesPerView: "auto",
     speed: 8000,
     grabCursor: true,
     spaceBetween: 16,
     //mousewheelControl: true,
     //keyboardControl: true,
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
   };
 
-  let swiper = new Swiper('.testimonials.one.swiper-container', swiperClients1);
-  $('.testimonials.one.swiper-container').hover(
+  let swiper = new Swiper(".testimonials.one.swiper-container", swiperClients1);
+  $(".testimonials.one.swiper-container").hover(
     function () {
       swiper.autoplay.stop();
     },
@@ -186,23 +201,23 @@ $(document).ready(function () {
       delay: 1,
       disableOnInteraction: false,
     },
-    slidesPerView: 'auto',
+    slidesPerView: "auto",
     speed: 10000,
     grabCursor: true,
     spaceBetween: 16,
     //mousewheelControl: true,
     //keyboardControl: true,
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
   };
 
   let swiper2 = new Swiper(
-    '.testimonials.two.swiper-container',
+    ".testimonials.two.swiper-container",
     swiperClients2
   );
-  $('.testimonials.two.swiper-container').hover(
+  $(".testimonials.two.swiper-container").hover(
     function () {
       swiper.autoplay.stop();
     },
@@ -217,37 +232,37 @@ $(document).ready(function () {
   /**
    * Video.
    */
-  $('.showVideo').on('click', function () {
-    const videoUrl = $(this).data('video-url');
+  $(".showVideo").on("click", function () {
+    const videoUrl = $(this).data("video-url");
 
-    $('#videoPlayer source').attr('src', videoUrl);
+    $("#videoPlayer source").attr("src", videoUrl);
 
-    $('#videoPlayer')[0].load();
-    $('#videoPlayer')[0].play();
+    $("#videoPlayer")[0].load();
+    $("#videoPlayer")[0].play();
 
-    $('#videoModal').css('display', 'block');
+    $("#videoModal").css("display", "block");
   });
 
-  $('#closeModal').on('click', function () {
-    $('#videoPlayer')[0].pause();
+  $("#closeModal").on("click", function () {
+    $("#videoPlayer")[0].pause();
 
-    $('#videoModal').css('display', 'none');
+    $("#videoModal").css("display", "none");
   });
   /**
    * Pricing.
    */
-  $('.pricing-options input').change(function () {
+  $(".pricing-options input").change(function () {
     let interval = $(this).val();
-    if ($('.pricing-options').hasClass('monthly')) {
-      $('.pricing-options').removeClass('monthly');
-      $('.pricing-options').addClass(interval);
-      $('.price .monthly').removeClass('selected');
-      $('.price .quaterly').addClass('selected');
+    if ($(".pricing-options").hasClass("monthly")) {
+      $(".pricing-options").removeClass("monthly");
+      $(".pricing-options").addClass(interval);
+      $(".price .monthly").removeClass("selected");
+      $(".price .quaterly").addClass("selected");
     } else {
-      $('.pricing-options').removeClass('quaterly');
-      $('.pricing-options').addClass(interval);
-      $('.price .quaterly').removeClass('selected');
-      $('.price .monthly').addClass('selected');
+      $(".pricing-options").removeClass("quaterly");
+      $(".pricing-options").addClass(interval);
+      $(".price .quaterly").removeClass("selected");
+      $(".price .monthly").addClass("selected");
     }
   });
 
@@ -255,36 +270,36 @@ $(document).ready(function () {
    * Pricing List.
    */
   let isTextVisible = true;
-  $('.pricing-more').on('click', function () {
+  $(".pricing-more").on("click", function () {
     if (isTextVisible) {
-      $(this).find(':eq(0)').text('Hide');
+      $(this).find(":eq(0)").text("Hide");
     } else {
-      $(this).find(':eq(0)').text('View more');
+      $(this).find(":eq(0)").text("View more");
     }
 
     isTextVisible = !isTextVisible;
-    $(this).toggleClass('show');
+    $(this).toggleClass("show");
     $(this).next().slideToggle();
   });
 
   /**
    * Frequently Asked Questions.
    */
-  $('.faq-trigger').on('click', function () {
-    $(this).next().toggleClass('show');
-    $(this).find('.faq-icon').toggleClass('rotate');
+  $(".faq-trigger").on("click", function () {
+    $(this).next().toggleClass("show");
+    $(this).find(".faq-icon").toggleClass("rotate");
   });
 
   /**
    * Header.
    */
-  const header = document.querySelector('.header');
+  const header = document.querySelector(".header");
 
-  window.addEventListener('scroll', () => {
+  window.addEventListener("scroll", () => {
     if (window.scrollY > 100) {
-      header.classList.add('scrolled');
+      header.classList.add("scrolled");
     } else {
-      header.classList.remove('scrolled');
+      header.classList.remove("scrolled");
     }
   });
 });
