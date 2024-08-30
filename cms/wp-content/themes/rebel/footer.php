e4e1318c1558439fca2904d631bac14205884f39
Daniela Leal <falfada@gmail.com>
2024-08-30 18:06:29 -0500

    Added minified JS file

--- cms/wp-content/themes/rebel/footer.php
+++ cms/wp-content/themes/rebel/footer.php
@@ -71,7 +71,7 @@
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <!-- Swiper JS -->
 <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
-<script src="<?= get_template_directory_uri() ?>/js/functions.js"></script>
+<script src="<?= get_template_directory_uri() ?>/js/functions.min.js"></script>
 </body>
 
 </html>
